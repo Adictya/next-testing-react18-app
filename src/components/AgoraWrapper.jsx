efe36107fde279f6ec58d51330006503b0c5d2a2
adictya <aditya.26101@gmail.com>
2022-12-22 16:54:20 +0530

    updated for join_method testing

--- src/components/AgoraWrapper.jsx
+++ src/components/AgoraWrapper.jsx
@@ -1,8 +1,19 @@
 import Agora from "@appbuilder/react";
+import { useEffect } from "react";
 
 const AgoraWrapper = () => {
+  useEffect(() => {
+    Agora.join("0b544efe-57f4-4727-b275-cefc85102997");
+  }, []);
   return (
     <div style={{ height: "100vh", width: "100vw", display: "flex" }}>
+      <button
+        onClick={() => {
+          Agora.join("0b544efe-57f4-4727-b275-cefc85102997");
+        }}
+      >
+        Join
+      </button>
       <Agora.View />
     </div>
   );

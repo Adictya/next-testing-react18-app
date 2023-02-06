47adcbe832ec0a83db78c43305d81146e46667e0
adictya <aditya.26101@gmail.com>
2023-02-06 16:28:27 +0530

    updated core

--- src/components/AgoraWrapper.jsx
+++ src/components/AgoraWrapper.jsx
@@ -1,19 +1,25 @@
 import Agora from "@appbuilder/react";
+import Link from "next/link";
 import { useEffect } from "react";
 
 const AgoraWrapper = () => {
   useEffect(() => {
-    Agora.join("0b544efe-57f4-4727-b275-cefc85102997");
+    Agora.join("30af1c5e-7860-484e-929f-5cab550c9d5b");
+    window.addEventListener("beforeunload", () => {
+      console.log("Unloading")
+      return false;
+    });
   }, []);
   return (
     <div style={{ height: "100vh", width: "100vw", display: "flex" }}>
       <button
         onClick={() => {
-          Agora.join("0b544efe-57f4-4727-b275-cefc85102997");
+          Agora.join("30af1c5e-7860-484e-929f-5cab550c9d5b");
         }}
       >
         Join
       </button>
+      <Link href="https://www.google.com">Join</Link>
       <Agora.View />
     </div>
   );
